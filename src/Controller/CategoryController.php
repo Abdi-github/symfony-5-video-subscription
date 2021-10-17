f10f90d628f0652ec3e7b9ec4a9cf95384a0ed19
abdigit <abdulkadirahmed1985@gmail.com>
2021-10-18 01:30:02 +0200

    fix header category dropdown

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,15 @@ class CategoryController extends AbstractController
     {
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    public function getCategories(CategoryRepository $cr)
+    {
+        $categories = $cr->findBy([], ['id' => 'ASC']);
+
+        return $this->render('common/_categories.html.twig', [
+            'categories' => $categories
         ]);
     }
 }
